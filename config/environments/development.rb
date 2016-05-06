616f1d149e2a7d2fe7740ab10062bd28270a1ed7
lauryndbrown <lauryndbrown@gmail.com>
2016-05-06 10:25:47 -0500

    Fixed logo problem. Not sure for phone

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -22,7 +22,7 @@ Rails.application.configure do
   
 
    config.action_mailer.default :charset => "utf-8"
-  config.action_mailer.smtp_settings = {
+   config.action_mailer.smtp_settings = {
     address:"smtp.gmail.com",
     port:587,
     domain: ENV["GMAIL_DOMAIN"],
